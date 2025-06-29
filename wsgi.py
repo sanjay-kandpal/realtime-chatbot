299db2ac715714ab7d71c068250272fe36dda4e8
sanjay <72781686+sanjay-kandpal@users.noreply.github.com>
2025-06-29 13:42:24 +0530

    eventlet fixed apply

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,8 @@
+import eventlet
+eventlet.monkey_patch()
+
+from flask import Flask
+from flask_socketio import SocketIO
 from app import app, socketio
 
 # Gunicorn will use socketio to handle the app
